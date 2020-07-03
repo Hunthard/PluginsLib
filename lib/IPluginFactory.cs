0641987cf4cc94b84108ab7e5277098ccd7934ed
Artyom Okhota <shpager@gmail.com>
2020-07-03 14:02:51 +0300

    Add project files.

---
+++ lib/IPluginFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl
+{
+    interface IPluginFactory
+    {
+        int PluginsCount { get; }
+        string[] GetPluginsNames { get; }
+        IPlugin GetPlugin(string pluginName);
+    }
+}
